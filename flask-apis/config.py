f8affdc4ccbde8f31f929f45f4d7146e53404ddb
neehar-pavan-chaluvadi <120152444+neehar-pavan-chaluvadi@users.noreply.github.com>
2022-12-14 00:46:39 +0530

    Update config.py

--- flask-apis/config.py
+++ flask-apis/config.py
@@ -3,8 +3,15 @@ from datetime import timedelta
 ''
 class Config:
     DEBUG = True
+    """Used for local db file
     basedir = '../' #change
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
+    """
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{student}:{password}@{host}'.format(
+        student='',
+        password='',
+        host='db-postgresql-is-5600-22-do-user-12767037-0.b.db.ondigitalocean.com:25060/neeharchaluvadi'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'neeharchaluvadi'
     JWT_ERROR_MESSAGE_KEY = 'message'
