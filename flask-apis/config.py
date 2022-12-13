517bfb896de3b17903c0695430b855be6e8c83ab
neehar-pavan-chaluvadi <120152444+neehar-pavan-chaluvadi@users.noreply.github.com>
2022-12-14 00:47:15 +0530

    Update config.py

--- flask-apis/config.py
+++ flask-apis/config.py
@@ -8,7 +8,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
     """
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{student}:{password}@{host}'.format(
-        student='',
+        student='neeharchaluvadi',
         password='',
         host='db-postgresql-is-5600-22-do-user-12767037-0.b.db.ondigitalocean.com:25060/neeharchaluvadi'
     )
